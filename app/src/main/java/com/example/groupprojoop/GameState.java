f1ef496c5fca6b2abbecbe3131a0e7a9fd181cbf
vividh masilamani <vividh0gamerr@gmail.com>
2026-04-03 23:35:08 +0300

    First

---
+++ app/src/main/java/com/example/groupprojoop/GameState.java
@@ -0,0 +1,15 @@
+package com.example.groupprojoop;
+
+import java.util.List;
+
+
+public  class GameState {
+    public List<CrewMember> quartersCrew;
+    public List<CrewMember> missionCrew;
+    public List<CrewMember> defeatedUnits;
+    public int globalMissionsPlayed;
+    public int globalVictories;
+    public int missionCounter;
+    public int successfulMissions;
+    public int failedMissions;
+}
